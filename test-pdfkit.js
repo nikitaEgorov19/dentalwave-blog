7de7ebd56ea8985b6d1687b2f12d0ce55ea28de0
Nikita Egorov <guccihighwaters@mail.ru>
2026-04-19 22:52:36 +0300

    Production clean: keep only server.js, public/, data/, Dockerfile, README

--- test-pdfkit.js
+++
@@ -1,7 +0,0 @@
-try {
-    const PDFKit = require('pdfkit');
-    console.log('PDFKit loaded, version:', PDFKit.version);
-} catch (e) {
-    console.error('PDFKit error:', e.message);
-    console.error(e.stack);
-}
--- test-write.js
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-fs.writeFileSync('test-write.txt', 'Test write at ' + new Date());
-console.log('Wrote to file');
